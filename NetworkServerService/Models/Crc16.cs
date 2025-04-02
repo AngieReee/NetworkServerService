841ead8eadc1ddef0b7b69212d37b25a5b3026e3
Snake <fireisnakei@gmail.com>
2025-04-02 12:58:46 +0300

    Добавьте файлы проекта.

---
+++ NetworkServerService/Models/Crc16.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkServerService.Models
+{
+    public class Crc16
+    {
+        public static ushort Calculate(byte[] data)
+        {
+            const ushort poly = 0xA001;
+            ushort crc = 0xFFFF;
+
+            foreach (byte b in data)
+            {
+                crc ^= b;
+                for (int i = 0; i < 8; i++)
+                {
+                    bool lsb = (crc & 1) == 1;
+                    crc >>= 1;
+                    if (lsb) crc ^= poly;
+                }
+            }
+            return crc;
+        }
+    }
+}
---
+++ NetworkServerService/Program.cs
@@ -0,0 +1,50 @@
+﻿using NetworkServerService.Services;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace NetworkServerService
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var server = new TcpListener(IPAddress.Any, 8080);
+            server.Start();
+
+            Debug.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                var client = await server.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+            using (client)
+            using (var stream = client.GetStream())
+            {
+                Debug.WriteLine("Новое подключение");
+
+                while (client.Connected)
+                {
+                    var buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    if (bytesRead > 0)
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Debug.WriteLine($"Клиент: {message}");
+
+                        var response = $"Сервер: {message}";
+                        var responseData = Encoding.UTF8.GetBytes(response);
+                        await stream.WriteAsync(responseData, 0, responseData.Length);
+                    }
+                }
+            }
+        }
+    }
+}
